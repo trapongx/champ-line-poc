49d3a56863793f1b7ab2279e994bd70d5db602f8
Teerapong Suksuwan <trapongx@gmail.com>
2020-06-17 14:56:13 +0700

    Add HTTPS certificate and add dual port web server (80 and 443 at the same time)

---
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/HttpPortConfiguration.kt
@@ -0,0 +1,28 @@
+package com.example.bot.spring.echo
+
+import org.apache.catalina.connector.Connector
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+open class HttpPortConfiguration {
+    @Value("\${http.port:80}")
+    var httpPort: Int = 80
+
+    @Bean
+    open fun servletContainer(): ServletWebServerFactory? {
+        val tomcat = TomcatServletWebServerFactory()
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector())
+        return tomcat
+    }
+
+    private fun createStandardConnector(): Connector? {
+        val connector = Connector("org.apache.coyote.http11.Http11NioProtocol")
+        connector.port = httpPort
+        return connector
+    }
+}
